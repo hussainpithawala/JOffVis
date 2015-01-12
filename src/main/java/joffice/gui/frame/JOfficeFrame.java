810e9c041ddfe2018916a323ddb87b45305ed46c
hussainp <hussainpithawala@hotmail.com>
2015-01-12 09:43:22 +0530

    Tabbed Pane layout for Summary/DocumentSummary Information
    1) Provided the tabbed pane layout for Summary/DocumentSummary Information
    2) Splitted the existing panes in desirable ratios

--- src/main/java/joffice/gui/frame/JOfficeFrame.java
+++ src/main/java/joffice/gui/frame/JOfficeFrame.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.JFileChooser;
@@ -19,9 +18,10 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
-import javax.swing.SwingUtilities;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
@@ -32,34 +32,60 @@ import javax.swing.tree.TreeSelectionModel;
 import joffice.gui.filter.ExtensionFileFilter;
 import joffice.gui.model.RecordTreeNode;
 import joffice.gui.model.RecordTreeTableModel;
-import joffice.poi.listener.MyPOIFSReaderListener;
 
+import org.apache.poi.hpsf.DocumentSummaryInformation;
+import org.apache.poi.hpsf.Property;
+import org.apache.poi.hpsf.PropertySetFactory;
+import org.apache.poi.hpsf.Section;
 import org.apache.poi.hpsf.SummaryInformation;
 import org.apache.poi.hssf.record.Record;
 import org.apache.poi.hssf.record.RecordFactory;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.eventfilesystem.POIFSReader;
+import org.apache.poi.poifs.eventfilesystem.POIFSReaderEvent;
+import org.apache.poi.poifs.eventfilesystem.POIFSReaderListener;
 import org.apache.poi.poifs.filesystem.DirectoryNode;
-import org.apache.poi.poifs.filesystem.Entry;
 import org.apache.poi.poifs.filesystem.NPOIFSFileSystem;
 import org.jdesktop.swingx.JXTreeTable;
 import org.jdesktop.swingx.treetable.TreeTableModel;
 
 @SuppressWarnings("serial")
 public class JOfficeFrame extends JFrame {
-  
+
   private static final String XLS = ".xls";
   private static final int DEFAULT_WIDTH = 800;
   private static final int DEFAULT_HEIGHT = 600;
-  
+
   private String lastOpenedDirectory = null;
   private JXTreeTable treeTable = new JXTreeTable();
-  private JTextArea descriptionArea = new JTextArea();
-  
+
   /**
    * A description pane holds the description area
    */
+  private JTextArea descriptionArea = new JTextArea();
   private JScrollPane descriptionPane = new JScrollPane(descriptionArea);
+
+  /**
+   * A Summary pane holds the summary area
+   */
+  private JTextArea summaryArea = new JTextArea();
+  private JScrollPane summaryPane = new JScrollPane(summaryArea);
+  
+  /**
+   * A Document summary pane holds the document summary area
+   */
+  private JTextArea documentSummaryArea = new JTextArea();
+  private JScrollPane documentSummaryPane = new JScrollPane(documentSummaryArea);
+
+  private JTabbedPane tabbedPane = new JTabbedPane(SwingConstants.TOP, JTabbedPane.SCROLL_TAB_LAYOUT);
+  
+  
+  /**
+   * Right Split Pane holds the summary information and descriptionPane
+   */
+  private JSplitPane rightSplitPane = 
+      new JSplitPane(JSplitPane.VERTICAL_SPLIT,descriptionPane, tabbedPane);
+
   /**
    * A tree table pane holds the treeTable for the records
    */
@@ -67,39 +93,52 @@ public class JOfficeFrame extends JFrame {
   /**
    * A main pane which is split horizontally for holding two panes
    */
-  private JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, treeTablePane, descriptionPane);
-  
+  private JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+      treeTablePane, rightSplitPane);
+
   /**
    * A content pane to hold the splitPane
    */
   private JPanel contentPanel = new JPanel();
-  
+
   public JOfficeFrame() {
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setVisible(true);
     setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setBounds(100, 100, 450, 300);
+
+    rightSplitPane.setOneTouchExpandable(true);
+    rightSplitPane.setResizeWeight(0.7f);
     
     splitPane.setOneTouchExpandable(true);
-      
+    splitPane.setResizeWeight(0.4f);
+    
     contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
     contentPanel.setLayout(new BorderLayout(0, 0));
     contentPanel.add(splitPane, BorderLayout.CENTER);
-    
-    splitPane.setResizeWeight(0.5f);
-    
+
+    add(contentPanel, BorderLayout.CENTER);
+
     addMenu();
     addTreeTable();
+    addTabs();
   }
 
+  private void addTabs() {
+    tabbedPane.add("Summary ", summaryPane);
+    tabbedPane.add("Document-Summary", documentSummaryPane);
+  }
+  
   private void addTreeTable() {
     descriptionArea.setEditable(false); // set textArea non-editable
-    descriptionPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-    
-    treeTable.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+    descriptionPane
+        .setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+    treeTable.getSelectionModel().setSelectionMode(
+        TreeSelectionModel.SINGLE_TREE_SELECTION);
     /**
-     * Add a TreeSelectionListener which listens for the selected node  
+     * Add a TreeSelectionListener which listens for the selected node
      */
     treeTable.addTreeSelectionListener(new TreeSelectionListener() {
       public void valueChanged(TreeSelectionEvent e) {
@@ -107,7 +146,8 @@ public class JOfficeFrame extends JFrame {
         TreePath path = treeTable.getTreeSelectionModel().getSelectionPath();
         if (path == null)
           return;
-        RecordTreeNode recordTreeNode = (RecordTreeNode)path.getLastPathComponent();
+        RecordTreeNode recordTreeNode = (RecordTreeNode) path
+            .getLastPathComponent();
         Record record = recordTreeNode.getRecord();
         if (record != null) {
           descriptionArea.setText(record.toString());
@@ -116,15 +156,8 @@ public class JOfficeFrame extends JFrame {
         }
       }
     });
-   
-    // A scroll pane for the table
-    treeTablePane = new JScrollPane(treeTable);
-    JSplitPane mainPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, treeTablePane, descriptionPane);
-    
-    mainPane.setDividerLocation(0.5);
-    add(mainPane, BorderLayout.CENTER);
   }
-  
+
   private void updateTreeTable(List<Record> records) {
     setTitle("Record Table");
     TreeTableModel treeTableModel = new RecordTreeTableModel(records);
@@ -156,11 +189,12 @@ public class JOfficeFrame extends JFrame {
     public void actionPerformed(ActionEvent event) {
       // prompt the user for a xls file
       JFileChooser chooser = new JFileChooser();
-      
+
       if (lastOpenedDirectory != null) {
         chooser.setCurrentDirectory(new File(lastOpenedDirectory));
       } else {
-        chooser.setCurrentDirectory(FileSystemView.getFileSystemView().getDefaultDirectory());
+        chooser.setCurrentDirectory(FileSystemView.getFileSystemView()
+            .getDefaultDirectory());
       }
       ExtensionFileFilter filter = new ExtensionFileFilter();
       filter.addExtension(XLS);
@@ -175,28 +209,28 @@ public class JOfficeFrame extends JFrame {
       }
     }
   }
+
   /**
    * Parse the xls file and populate the table
    */
   private void loadXLSFile(String xlsFile) {
     try {
-      System.out.println("SummaryInformation Default Stream Name " + SummaryInformation.DEFAULT_STREAM_NAME);
-      POIFSReader r = new POIFSReader(); 
-      r.registerListener(new MyPOIFSReaderListener(), SummaryInformation.DEFAULT_STREAM_NAME);
-      r.read(new FileInputStream(xlsFile));
-       
+      POIFSReader summaryReader = new POIFSReader();
+      summaryReader.registerListener(new SummaryListener(),
+          SummaryInformation.DEFAULT_STREAM_NAME);
+      summaryReader.read(new FileInputStream(xlsFile));
+
+      POIFSReader documentSummaryReader = new POIFSReader();
+      documentSummaryReader.registerListener(new DocumentSummaryListener(),
+          DocumentSummaryInformation.DEFAULT_STREAM_NAME);
+      documentSummaryReader.read(new FileInputStream(xlsFile));
+      
       NPOIFSFileSystem fs = new NPOIFSFileSystem(new File(xlsFile));
-      
+
       DirectoryNode directoryNode = fs.getRoot();
-      Iterator<Entry> entries = directoryNode.getEntries();
-      while (entries.hasNext()) {
-        Entry entry = entries.next();
-        System.out.println(entry.getName());
-      }
-      
-      String workbookName = HSSFWorkbook
-          .getWorkbookDirEntryName(directoryNode);
-      
+
+      String workbookName = HSSFWorkbook.getWorkbookDirEntryName(directoryNode);
+
       InputStream stream = directoryNode
           .createDocumentInputStream(workbookName);
       List<Record> records = RecordFactory.createRecords(stream);
@@ -207,6 +241,55 @@ public class JOfficeFrame extends JFrame {
       JOptionPane.showMessageDialog(null, "Unable to open file");
       e.getMessage();
       e.printStackTrace();
+    }
+  }
+
+  private class SummaryListener implements POIFSReaderListener {
+    public void processPOIFSReaderEvent(POIFSReaderEvent event) {
+      StringBuilder summary = new StringBuilder();
+      SummaryInformation si = null;
+      try {
+        si = (SummaryInformation) PropertySetFactory.create(event.getStream());
+      } catch (Exception ex) {
+        throw new RuntimeException("Property set stream \"" + event.getPath()
+            + event.getName() + "\": " + ex);
+      }
+      summary.append("Title " + si.getTitle() + "\n");
+      summary.append("Last Saving application " + si.getApplicationName()
+          + "\n");
+      summary.append("OS Version " + si.getOSVersion() + "\n");
+      summary.append("Rev " + si.getRevNumber() + "\n");
+      summary.append("Original Author " + si.getAuthor() + "\n");
+      summary.append("Last Author " + si.getLastAuthor() + "\n");
+      summary.append("Last Saved Date " + si.getLastSaveDateTime() + "\n");
+      summary.append("Creation Date " + si.getCreateDateTime() + "\n");
+      summaryArea.setText(summary.toString());
+    }
+  }
+  
+  private class DocumentSummaryListener implements POIFSReaderListener {
+    public void processPOIFSReaderEvent(POIFSReaderEvent event) {
+      StringBuilder documentSummary = new StringBuilder();
+      DocumentSummaryInformation dsi = null;
+      try {
+        dsi = (DocumentSummaryInformation) PropertySetFactory.create(event.getStream());
+      } catch(Exception ex) {
+        throw new RuntimeException("Property set stream \"" + event.getPath()
+            + event.getName() + "\": " + ex);        
+      }
+      for (Section section : dsi.getSections()) {
+        for (Property property : section.getProperties()) {
+          documentSummary.append("--------------------------" + "\n");
+          documentSummary.append("ID: " + property.getID() + "\n");
+          documentSummary.append("Value: " + property.getValue() + "\n");
+          documentSummary.append("Type: " + property.getType() + "\n");
+          documentSummary.append("--------------------------" + "\n");
+        }
+        documentSummary.append("\n");
+      }
+      // TODO Current Document Summary only prints properties
+      // Need to fill this area with some relevant information
+      // documentSummaryArea.setText(documentSummary.toString());
     }
   }
 }
